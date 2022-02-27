cdb61eadb9bc3b85861e7cd780d58942b60ad982
shaked <shakedrosenblat@gmail.com>
2022-02-27 15:58:58 +0200

    finalize

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ const options = {
         format: "dd-MM-yyyy",
     },
     series: {
-        0: { color: 'red' },
-        1: { color: 'blue' },
-        2: { color: 'green' },
+        0: {color: 'red'},
+        1: {color: 'blue'},
+        2: {color: 'green'},
     }
 };
 
@@ -23,25 +23,30 @@ function App() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([] as any[]);
+    const [invalidData, setInvalidData] = useState(false)
     const [startDate, setStartDate] = useState(new Date("2021-06-01T00:00:00.000Z"));
     const [endDate, setEndDate] = useState(new Date("2021-12-31T00:00:00.000Z"));
 
     useEffect(() => {
-        fetch(`http://localhost:3000/transactions?fromDate=${startDate.toISOString()}&toDate=${endDate.toISOString()}`, {method: 'GET'})
-            .then(res => res.json())
-            .then(
-                (result: TransactionDto[]) => {
-                    const data = [['', 'income', 'outcome', 'clear'], ...result.map(res => ([new Date(res.date), res.income, res.outcome, res.clear]))]
-                    console.log('now got data')
-                    setItems(data);
-                    setIsLoaded(true);
-                },
-                (error) => {
-                    console.log(error)
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
+        if (!(startDate.getTime() < endDate.getTime())) {
+            setInvalidData(true)
+        } else {
+            setInvalidData(false);
+            fetch(`http://localhost:3000/transactions?fromDate=${startDate.toISOString()}&toDate=${endDate.toISOString()}`, {method: 'GET'})
+                .then(res => res.json())
+                .then(
+                    (result: TransactionDto[]) => {
+                        const data = [['', 'income', 'outcome', 'clear'], ...result.map(res => ([new Date(res.date), res.income, res.outcome, res.clear]))]
+                        setItems(data);
+                        setIsLoaded(true);
+                    },
+                    (error) => {
+                        console.log(error)
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                )
+        }
     }, [startDate, endDate])
 
     if (error) {
@@ -51,15 +56,19 @@ function App() {
     } else {
         return (
             <div className={"content"}>
-                <Header dates={{startDate: startDate, endDate: endDate}} handleStartDateSelect={setStartDate} handleEndDateSelect={setEndDate}/>
+                <Header dates={{startDate: startDate, endDate: endDate}} handleStartDateSelect={setStartDate}
+                        handleEndDateSelect={setEndDate}/>
+                {invalidData ?
+                    <div className={"error"}>Please enter valid data. Start date should come before end date.</div> :
+                    <div/>}
                 <br/>
                 <Chart
+
                     chartType="Line"
                     width="100%"
                     height="400px"
                     data={items}
                     options={options}
-                    onLoad={(_) => {console.log('now onLoad')}}
                 />
                 <br/>
                 <Footer/>
